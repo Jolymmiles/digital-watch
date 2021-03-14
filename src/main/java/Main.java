eade8174280e7505c24ac4030bdcc8dbbfb9dbf0
Boris Moiseev <bmoiseev@digital-spirit.ru>
2021-03-15 00:10:36 +0400

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        Scanner sc = new Scanner(System.in);
-
-        int number = sc.nextInt();
-        int hours = (number % 1440) / 60;
-        int minutes = number % 60;
-        System.out.printf("%d:%d", hours, minutes);
-
-        sc.close();
     }
 }
