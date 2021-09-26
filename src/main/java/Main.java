18d10d2579ea70cca06a25a2e76fb1228f7cb7db
Валентин <valmak04@gmail.com>
2021-09-26 22:26:52 +0400

    Done 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int h = n / 60, s = n - (60 * h);
-        System.out.println(h + ":" + s);
+        if (n < 1440) {
+            int h = n / 60, s = n - (60 * h);
+            System.out.println(h + ":" + s);
+        }else {
+            int days = n - (n / 1440) * 1440;
+            int h = days / 60, s = days - (60 * h);
+            System.out.println(h + ":" + s);
+
+        }
+
 
     }
 }
