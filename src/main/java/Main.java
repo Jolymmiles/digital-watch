eccbc0df1a0cdc52a045df03ddcfb27608237d17
Валентин <valmak04@gmail.com>
2021-09-26 22:09:40 +0400

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int h = n / 60, s = n - (60 * h);
+        System.out.println(h + ":" + s);
+
     }
 }
